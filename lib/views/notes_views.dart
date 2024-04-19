e518c1774878f2b041e1d289ab3b45d2dca18e02
Your hsmalafifi <hsmalafifi@gmail.com>
2024-04-19 14:14:10 -0700

    show modeal boottom sheet

--- lib/views/notes_views.dart
+++ lib/views/notes_views.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:noot_app/views/Widgets/add_note_bottom_sheet.dart';
 
 import 'Widgets/Notes_view_body.dart';
 
@@ -7,8 +8,20 @@ class NotesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: NotesViewBobdy() ,
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet(
+              context: context,
+              builder: (context) {
+                return const AddNoteBottomSheet();
+              });
+        },
+        child: const Icon(
+          Icons.add,
+        ),
+      ),
+      body: const NotesViewBobdy(),
     );
   }
 }
